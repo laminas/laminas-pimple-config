8735e02ee69ac52ed83da4bbba49aa6398b6a75c
Marco Pivetta <ocramius@gmail.com>
2022-12-21 04:03:36 +0100

    Improved `Config` type definitions/documentation, re-generated baseline
    Note: we did **not** remove redundant checks, since the primary purpose of the `Config`
    object is to act as a small anti-corruption layer between user-provider configuration
    and our own internal logic.
    
    This may be changed in a new major version, but for now, reporting these redundant conditions
    is a safer choice.

--- src/Config.php
+++ src/Config.php
@@ -15,11 +15,24 @@ use function is_int;
 use function is_string;
 use function sprintf;
 
+/**
+ * @psalm-type DependenciesConfig = array{
+ *     shared_by_default?: bool,
+ *     services?: array<string, mixed>,
+ *     factories?: array<string, callable(PsrContainer, string): mixed|class-string>,
+ *     delegators?: array<string, array<class-string>>,
+ *     invokables?: array<string, class-string>,
+ *     aliases?: array<string, string>,
+ *     extensions?: array<string, array<class-string>>,
+ *     ...
+ * }
+ */
 class Config implements ConfigInterface
 {
-    /** @var array */
+    /** @var array{dependencies?: DependenciesConfig, ...} */
     private $config;
 
+    /** @psalm-param array{dependencies?: DependenciesConfig, ...} $config */
     public function __construct(array $config)
     {
         $this->config = $config;
@@ -29,6 +42,7 @@ class Config implements ConfigInterface
     {
         $container['config'] = $this->config;
 
+        /** @var DependenciesConfig $dependencies */
         $dependencies = [];
         if (
             isset($this->config['dependencies'])
@@ -47,6 +61,7 @@ class Config implements ConfigInterface
         $this->injectExtensions($container, $dependencies);
     }
 
+    /** @param DependenciesConfig $dependencies */
     private function injectServices(Container $container, array $dependencies): void
     {
         if (
@@ -63,6 +78,7 @@ class Config implements ConfigInterface
         }
     }
 
+    /** @param DependenciesConfig $dependencies */
     private function injectFactories(Container $container, array $dependencies): void
     {
         if (
@@ -100,6 +116,7 @@ class Config implements ConfigInterface
         }
     }
 
+    /** @param DependenciesConfig $dependencies */
     private function injectInvokables(Container $container, array $dependencies): void
     {
         if (
@@ -138,6 +155,7 @@ class Config implements ConfigInterface
         }
     }
 
+    /** @param DependenciesConfig $dependencies */
     private function injectAliases(Container $container, array $dependencies): void
     {
         if (
@@ -152,6 +170,7 @@ class Config implements ConfigInterface
         }
     }
 
+    /** @param DependenciesConfig $dependencies */
     private function injectExtensions(Container $container, array $dependencies): void
     {
         if (
